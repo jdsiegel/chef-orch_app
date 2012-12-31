75eb80136f9e66895a46baa268084d03846ac900
Jeff Siegel <jdsiegel@gmail.com>
2012-12-31 03:46:14 -0700

    move the test attributes into jamie config
    The test recipe is now just a shell, but might be needed for future
    configuration

--- test/cookbooks/test_setup/recipes/default.rb
+++ test/cookbooks/test_setup/recipes/default.rb
@@ -1,17 +1,3 @@
 extend OrchAppTest
 
-node['orch_app']['apps'] = [
-  {
-    'name'         => 'app1',
-    'user'         => 'vagrant',
-    'ruby_version' => '1.9.3-p327',
-    'processes'    => [['all', '1']],
-  },
-  #{
-    #'name'         => 'app2',
-    #'user'         => 'vagrant',
-    #'ruby_version' => '1.8.7-p370'
-  #},
-]
-
 require_recipe "orch_app"
