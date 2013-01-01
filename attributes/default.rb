445892da90990b5a137afa2d2a8b8189b2d35aad
Jeff Siegel <jdsiegel@gmail.com>
2013-01-01 02:48:14 -0700

    update to chruby v0.3.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['orch_app']['apps'] = []
-default['orch_app']['chruby']['version']       = '0.2.5'
-default['orch_app']['chruby']['checksum']      = 'f6ae5620f8ab760feb22ad607d75490041eec5ecd46d58e71891ea3d4e43f738'
+default['orch_app']['chruby']['version']       = '0.3.1'
+default['orch_app']['chruby']['checksum']      = '78ca15a1b275e522c324e9a5b09894d8884a7e2e7cf93dfe70bc8921bb5b57a1'
 default['orch_app']['chruby']['force_install'] = false
 default['orch_app']['bundler']['version']      = '1.2.2'
