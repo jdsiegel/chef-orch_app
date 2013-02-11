f13d87ea174b9bd96cb6b9b8899d405cce3818bb
Jeff Siegel <jdsiegel@gmail.com>
2013-02-11 00:43:53 -0700

    bump default bunder version to 1.2.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default['orch_app']['apps'] = []
 default['orch_app']['chruby']['version']       = '0.3.1'
 default['orch_app']['chruby']['checksum']      = '78ca15a1b275e522c324e9a5b09894d8884a7e2e7cf93dfe70bc8921bb5b57a1'
 default['orch_app']['chruby']['force_install'] = false
-default['orch_app']['bundler']['version']      = '1.2.2'
+default['orch_app']['bundler']['version']      = '1.2.3'
 default['orch_app']['rubies_path']             = '/opt/rubies'
