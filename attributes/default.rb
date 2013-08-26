859e5deea4a7b59a7ece468ef2d761e99bcdf24c
Jeff Siegel <jdsiegel@gmail.com>
2013-08-26 15:01:24 -0600

    updated default bundler version to 1.3.5

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['orch_app']['apps'] = []
-default['orch_app']['bundler']['version']      = '1.2.3'
+default['orch_app']['bundler']['version']      = '1.3.5'
 default['orch_app']['rubies_path']             = '/opt/rubies'
